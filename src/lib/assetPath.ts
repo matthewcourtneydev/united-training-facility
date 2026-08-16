38d78367d78a4e031e7129991f2c2e57dec6ff29
Matthew Courtney <mdcdev23@gmail.com>
2026-08-16 10:14:33 -0400

    Prepare site for custom domain

--- src/lib/assetPath.ts
+++ src/lib/assetPath.ts
@@ -1,8 +1,3 @@
 export function assetPath(path: string) {
-    const basePath =
-      process.env.NODE_ENV === "production"
-        ? "/united-training-facility"
-        : "";
-  
-    return `${basePath}${path}`;
+    return path;
   }
